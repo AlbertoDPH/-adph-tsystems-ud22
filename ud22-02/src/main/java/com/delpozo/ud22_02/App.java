c16eb6865351c6d92ce15d2891837afecef3abb3
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-04-16 18:17:17 +0200

    Creacion ejercicio 2
    UD02-02

---
+++ ud22-02/src/main/java/com/delpozo/ud22_02/App.java
@@ -0,0 +1,13 @@
+package com.delpozo.ud22_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
