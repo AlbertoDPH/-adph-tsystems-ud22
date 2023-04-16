39b7884ab5a7968a85724837dea5b3e6a240d0c7
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-04-16 12:39:32 +0200

    Cambios en  MainApp
    Eliminar vistas comentadas en clase principal

--- UD22_MVC/src/main/java/com/delpozo/UD22_MVC/MainApp.java
+++ UD22_MVC/src/main/java/com/delpozo/UD22_MVC/MainApp.java
@@ -7,27 +7,20 @@ import com.delpozo.UD22_MVC.vista.VistaPrincipal;
 import javax.swing.*;
 
 public class MainApp {
-    public static void main(String[] args) {
-        try {
-            ClienteDAO clienteDAO = new ClienteDAO();
+	public static void main(String[] args) {
+		try {
+			ClienteDAO clienteDAO = new ClienteDAO();
 
-            // Crear la ventana principal
-            VistaPrincipal vistaPrincipal = new VistaPrincipal();
+			// Crear la ventana principal
+			VistaPrincipal vistaPrincipal = new VistaPrincipal();
 
-            // Crear el controlador de la ventana principal
-            ControlVistaPrincipal controlVistaPrincipal = new ControlVistaPrincipal(vistaPrincipal, clienteDAO);
+			// Crear el controlador de la ventana principal
+			ControlVistaPrincipal controlVistaPrincipal = new ControlVistaPrincipal(vistaPrincipal, clienteDAO);
 
-            // Hacer que la ventana principal sea visible
-//            SwingUtilities.invokeLater(new Runnable() {
-//                public void run() {
-                    vistaPrincipal.setVisible(true);
-//                }
-//            });
+			vistaPrincipal.setVisible(true);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
